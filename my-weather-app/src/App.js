2bb25d8c920032fef7ac743be117b39e8d6b4733
Jeremias Santochi <jeremiassantochi@gmail.com>
2020-12-24 17:48:19 -0300

    ternary change on app className

--- my-weather-app/src/App.js
+++ my-weather-app/src/App.js
@@ -58,7 +58,11 @@ function App() {
   };
 
   return (
-    <div className="app">
+    <div className={(typeof weather.main != "undefined")
+        ?((weather.main.temp > 15)
+          ? 'app warm'
+          : 'app')
+        : 'app'}>
       <main>
         <div className="search-box">
           <input
@@ -73,14 +77,14 @@ function App() {
         {typeof weather.main != 'undefined' ? (
           <div>
             <div className="location-box">
-            <div className="location">{weather.name}, {weather.sys.country}</div>
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
               <div className="date">{dateBuilder(new Date())}</div>
             </div>
             <div className="weather-box">
-              <div className="temp">
-              {Math.round(weather.main.temp)}
-              </div>
-              <div className="weather">Sunny</div>
+              <div className="temp">{Math.round(weather.main.temp)}°c</div>
+              <div className="weather">{weather.weather[0].main}</div>
             </div>
           </div>
         ) : (
