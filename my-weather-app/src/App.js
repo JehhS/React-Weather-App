2b57c13beef656855d1211e442cb742fd87a4ea1
Jeremias Santochi <jeremiassantochi@gmail.com>
2020-12-24 01:14:47 -0300

    Create a const for the api

--- my-weather-app/src/App.js
+++ my-weather-app/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+const api = {
+  key: 'b2909c4c326c62a9098e46623a7243a3',
+  base: 'https://api.openweathermap.org/data/2.5/',
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola Mundo 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
